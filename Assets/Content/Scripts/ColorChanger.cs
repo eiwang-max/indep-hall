f12258fa5104b2f3eb56d415e567f3dceccc22be
eiwang-max <eiwang@baldwinschool.org>
2026-08-06 19:48:21 -0400

    extra changes

---
+++ Assets/Content/Scripts/ColorChanger.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.UI;
+
+ 
+
+public class ColorChanger : MonoBehaviour
+
+{
+
+   public bool changeColor;
+
+   private string colorName;
+
+   
+
+   public List<Image> images;
+
+}
